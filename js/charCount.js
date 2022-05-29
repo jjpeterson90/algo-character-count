0e33d485f61702799dd7d213e811cd61e53d2309
Justin <jjpeterson90@hotmail.com>
2022-05-29 13:49:20 -0500

    complete

--- js/charCount.js
+++ js/charCount.js
@@ -1,3 +1,24 @@
-exports.charCount = function() {
+exports.charCount = function(str) {
+    str = str.replaceAll(' ', '');
+    let countObj = {};
+    let result = [];
 
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] in countObj === false) {
+            countObj[str[i]] = 1;
+        } else {
+            countObj[str[i]] += 1;
+        }
+    }
+    
+    objLength = Object.entries(countObj).length;
+    for (let i = 0; i < objLength; i++) {
+        result[i] = Object.entries(countObj)[i];
+    }
+
+    let sortedResult = result.sort((a,b) => {
+        return a[1] === b[1] ? a[0].charCodeAt(0) - b[0].charCodeAt(0) : b[1] - a[1]
+    })
+
+    return sortedResult
 };
